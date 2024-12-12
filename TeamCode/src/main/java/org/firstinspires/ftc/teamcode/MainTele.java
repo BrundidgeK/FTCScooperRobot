c641cf55623cc3a51387295d026e825d0ff0afb9
PlagueDoctorSadMan <karakashg@franklinacademy.org>
2024-12-12 18:34:34 -0500

    d

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTele.java
@@ -11,16 +11,15 @@ public class MainTele extends LinearOpMode {
         ArmController arm = new ArmController(hardwareMap);
         waitForStart();
 
-        while(opModeIsActive()){
-            arm.setServos(new double[]{1,1,1});
-            telemetry.addLine("1");
-            telemetry.update();
-            sleep(5000);
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                arm.setServos(new double[]{1, 0, 0});
+            }
+            else if (gamepad1.b) {
+                arm.setServos(new double[]{0, 0, 0});
+            }
 
-            arm.setServos(new double[]{0,0,0});
-            telemetry.addLine("0");
-            telemetry.update();
-            sleep(5000);
         }
     }
 }
+
